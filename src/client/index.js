d1465c7399fb8b048e961f464146fc761fcdfa72
Balerion300 <39340548+Balerion300@users.noreply.github.com>
2025-09-05 18:30:51 +0200

    feat: add local echo toggle

--- src/client/index.js
+++ src/client/index.js
@@ -101,6 +101,7 @@ class Client {
       // terminal settings
       terminalWidth: 100,
       terminalAutoScroll: true,
+      showLocalEcho: true,
       // upload editor
       decompileEditor: true,
       decompileKey: 'FugueEdit > ',
@@ -1435,7 +1436,9 @@ class Client {
 
     cmd = this.filterUnicode(cmd);
     this.sendMacro(cmd);
-    this.scrollIfNeeded(() => this.appendMessage('localEcho', cmd));
+    if (this.settings.showLocalEcho) {
+      this.scrollIfNeeded(() => this.appendMessage('localEcho', cmd));
+    }
     this.saveRecallHistory();
   }
   
